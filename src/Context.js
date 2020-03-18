c900e6788c30156a2b598f1ff5ef7bdf730e8f9a
Lee Rogers <edwinleerogers@gmail.com>
2020-03-18 13:30:19 -0700

    add localstorage capability

--- src/Context.js
+++ src/Context.js
@@ -2,14 +2,24 @@ import React, {useState, useEffect} from "react";
 const Context = React.createContext();
 
 function ContextProvider(props) {
-  const [pics, setPics] = useState([]);
-  const [cart, setCart] = useState([]);
+  const [pics, setPics] = useState(JSON.parse(localStorage.getItem("pics")) || []);
+  const [cart, setCart] = useState(JSON.parse(localStorage.getItem("cart")) || []);
 
   useEffect(() => {
-      fetch("https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json")
-        .then(res => res.json())
-        .then(pictures => setPics(pictures))
+      if (!pics.length) {
+        fetch("https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json")
+          .then(res => res.json())
+          .then(pictures => setPics(pictures))
+      }
   }, [])
+
+  useEffect(() => {
+    localStorage.setItem("pics", JSON.stringify(pics));
+  }, [pics])
+  
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart])
 
   function toggleFavorite(id) {
     setPics(pics => pics.map(pic => {
